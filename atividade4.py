f75980c58a1018b64b706975fbff39e0941c0db5
Apolonio Santiago Junior <apolocomputacao@gmail.com>
2021-12-07 11:59:07 -0300

    Update atividade4.py

--- atividade4.py
+++ atividade4.py
@@ -3,6 +3,11 @@
 ATIVIDADE: Realizar uma busca de vagas no LINKEDIN passando um determinado tipo.
 '''
 
+#!/usr/bin/python3
+'''
+ATIVIDADE: Realizar uma busca de vagas no LINKEDIN passando um determinado tipo.
+'''
+
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import time
@@ -12,26 +17,26 @@ class RoboEmprego():
         self.webdriver = webdriver.Firefox()
 
     def busca(self, busca, paginas):
-        videos = []
+        vagas = []
         pagina =1
-        url ="https:/br.linkedin.com/jobs/search?keywords=%s" % busca
+
+        url ="https:/br.linkedin.com/jobs/search?keywords=%s"
         self.webdriver.get(url)
 
-        while pagina <= paginas:
-            titulos = self.webdriver.find_elements_by_xpath("//a[@id='aria-label']")
-            for titulo in titulos:
-                if titulo.text not in videos:
-                    print("Empregos encontrados: %s" % titulo.text) 
-                    videos.append(titulo.text)
-            self.proxima_pagina(pagina)
-            pagina += 1
+      
+        campo = self.webdriver.find_element_by_xpath("//input[@aria-label='Pesquisar cargos ou empresas']")
+        campo.send_keys(busca)
+        campo.send_keys(Keys.ENTER)
+        time.sleep(3)
 
-    def proxima_pagina(self, pagina):
-        print("Mundando para a página %s" % (pagina + 1))
-        bottom = pagina * 10000
-        self.webdriver.execute_script("window.scroll(0,%s);" % bottom)
-        time.sleep(5)
+        lista = self.webdriver.find_elements_by_xpath("//ul[@class='jobs-search__results-list']")
+        for elemento in lista:
+            itens =  elemento.find_elements_by_tag_name("li")
+            for item in itens:
+                nome = item.find_element_by_tag_name("h3")
+                print("Vaga encontrada: %s" % nome.text)
 
 bot = RoboEmprego()
 #Passando parametro para buscar o tipo de emprego
-bot.busca("financeiro",5)
+bot.busca("financeiro",2)
+
