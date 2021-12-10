da3d29761a98d8cc3a460eb2cb173e559c7b5c2f
Apolonio Santiago Junior <apolocomputacao@gmail.com>
2021-12-10 10:27:57 -0300

    Create atividade5.py

---
+++ atividade5.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+'''
+RESOLVER O CAPTION passando um CPF ou CNPJ
+http://servicos.receita.fazenda.gov.br/servicos/cnpjreva/Cnpjreva_Solicitacao_CS.asp
+https://programadorviking.com.br/metodo-format-em-python/
+
+'''
+
+
