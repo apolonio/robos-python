e2cb83f06b6ad5135fd019b3987044d2775c014f
Apolonio Santiago Junior <apolocomputacao@gmail.com>
2021-12-13 11:34:11 -0300

    Create atividade07_facebook.py

---
+++ atividade07_facebook.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+
+driver = webdriver.Firefox()
+driver.maximize_window()
+driver.implicitly_wait(20)
+driver.get("https://www.facebook.com/login")
+
+
+email = input("Seu email: ")
+senha = input("Senha: ")
+
+login = driver.find_element(By.XPATH,"//*[@id='email']")
+login.send_keys(email)
+
+login = driver.find_element(By.XPATH,"//*[@id='pass']")
+login.send_keys(senha)
+
+login.send_keys(Keys.ENTER)
